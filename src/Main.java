aeb501fbeb9e25299373867d2f6d9fd3bb96cf39
AjaysuryaS <114158396+AjaysuryaS@users.noreply.github.com>
2023-08-16 09:12:36 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 
-
-
 class InsufficientFundsException extends Exception {
     public InsufficientFundsException(String message) {
         super(message);
@@ -45,49 +43,63 @@ class ATM {
     }
 }
 
-public class Main {
-    public static void main(String[] args) {
+class IndianBankATM extends ATM {
+    public IndianBankATM(double initialBalance) {
+        super(initialBalance);
+    }
+
+    public void initialize() throws InvalidAmountException, InsufficientFundsException {
         Scanner scanner = new Scanner(System.in);
-        double initialBalance = 4000.0;
-        ATM atm = new ATM(initialBalance);
+        int userChoice, inputAmount;
 
-        while (true) {
-            System.out.println("ATM Menu:");
-            System.out.println("1. Check Balance");
-            System.out.println("2. Deposit");
-            System.out.println("3. Withdraw");
-            System.out.println("4. Exit");
-            System.out.print("Enter choice: ");
-            int choice = scanner.nextInt();
+        do {
+            System.out.println("________________________________");
+            System.out.println("1 - Deposit Amount");
+            System.out.println("2 - Withdraw Amount");
+            System.out.println("3 - Check Balance");
+            System.out.println("4 - Exit");
+            System.out.println("________________________________");
+            System.out.print("Enter your preferred option ... ");
+            userChoice = Integer.parseInt(scanner.nextLine());
 
-            switch (choice) {
+            switch (userChoice) {
                 case 1:
-                    System.out.println("Current Balance: " + atm.checkBalance());
+                    System.out.print("Enter the amount to be deposited ... Rs.");
+                    inputAmount = Integer.parseInt(scanner.nextLine());
+                    if (inputAmount <= 0) {
+                        throw new InvalidAmountException("Amount must be greater than 0.");
+                    }
+                    deposit(inputAmount);
                     break;
+
                 case 2:
-                    System.out.print("Enter amount to deposit: ");
-                    double depositAmount = scanner.nextDouble();
-                    try {
-                        atm.deposit(depositAmount);
-                    } catch (InvalidAmountException e) {
-                        System.out.println("Error: " + e.getMessage());
+                    System.out.print("Enter the amount to be withdrawn ... Rs.");
+                    inputAmount = Integer.parseInt(scanner.nextLine());
+                    if (inputAmount <= 0) {
+                        throw new InvalidAmountException("Amount must be greater than 0.");
                     }
+                    withdraw(inputAmount);
                     break;
+
                 case 3:
-                    System.out.print("Enter amount to withdraw: ");
-                    double withdrawAmount = scanner.nextDouble();
-                    try {
-                        atm.withdraw(withdrawAmount);
-                    } catch (InsufficientFundsException | InvalidAmountException e) {
-                        System.out.println("Error: " + e.getMessage());
-                    }
+                    System.out.println("Your balance is Rs. " + checkBalance());
                     break;
+
                 case 4:
                     System.out.println("Exiting ATM...");
                     System.exit(0);
+
                 default:
                     System.out.println("Invalid choice. Choose a valid option.");
             }
-        }
+        } while (true);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws InvalidAmountException, InsufficientFundsException {
+        double initialBalance = 4000.0;
+        IndianBankATM indianBankATM = new IndianBankATM(initialBalance);
+        indianBankATM.initialize();
     }
 }
